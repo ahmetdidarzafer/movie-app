215531e448ae53f9607681f36699f20d3e8ee193
ahmetdidarzafer <ahmetdidarzafer@gmail.com>
2024-08-09 11:36:57 +0300

    Update tmdb.service

--- src/tmdb/tmdb.service.ts
+++ src/tmdb/tmdb.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { map } from 'rxjs/operators';
 import { PopularMoviesDto } from './dto/popular-movies.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TmdbService {
-  private readonly apiKey: string = 'b0f26a52c899527a0ae8bd2aa6a05329';
+  private readonly apiKey: string = this.configService.get<string>('API_KEY');
   private readonly baseUrl: string = 'https://api.themoviedb.org/3';
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService, private configService : ConfigService) {}
 
   getMovieDetails(movieId: number) {
     const url = `${this.baseUrl}/movie/${movieId}?api_key=${this.apiKey}`;
